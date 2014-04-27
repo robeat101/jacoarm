a35ba3dbeb57ba9313d17573191f5e0201aeaf17
Rohit Bansal <rohit@bansalrohit.com>
2014-04-27 14:01:18 -0400

    Have FK working with minor error in result (calculation and rounding most likely)

--- src/jacofk.py
+++ src/jacofk.py
@@ -12,9 +12,8 @@ from numpy.linalg import pinv
 from math import *
 from sensor_msgs.msg import JointState
 import tf
+from jacoTF import *
 
-
-#import jacobian as J
 
 """
 The purpose of this node is to read current joint angles from the jaco arm
@@ -22,55 +21,41 @@ and publish the cartesian coordinates of the end effector of the JACO arm.
 """
 
 
-class jacofk:
-    
-    D1 = 0.2755
-    D2 = 0.4100
-    D3 = 0.2073
-    D4 = 0.0743
-    D5 = 0.0743
-    D6 = 0.1687
-    e2 = 0.0098
-    
-    aa = (11.0*pi)/72.0
-    ca = cos(aa)
-    sa = sin(aa)
-    c2a = cos(2*aa)
-    s2a = sin(2*aa)
-    d4b = (D3 + sa/s2a *D4)
-    d5b = (sa/s2a*D4 + sa/s2a *D5)
-    d5b = (sa/s2a*D4 + sa/s2a *D5)
-    d6b =(sa/s2a*D5 + D6)
-    
-    alpha = [pi/2, pi, pi/2, 2 * aa, 2 * aa, pi]
-    a = [0, D2, 0, 0, 0, 0]
-    d = [D1, 0, -e2, -d4b, -d5b, -d6b]
-    
+class jacofk(jacoTF):
+
     def calcFK(self, msg):
         joint_val = []
         for i in xrange(0,6):
             joint_val.append(msg.position[i])
         q = mat(joint_val)
-        q = self.jacotoDH(q)
-        T = list()
-        T_final = mat(identity(4))
-        for i in xrange(0,6):
-            T.append(self.transform(q.item(i),
-                           jacofk.a[i],
-                           jacofk.alpha[i],
-                           jacofk.d[i]))
-        
-        T_final = self.TBtoJB * self.TJBtoJ1 * T[0]*T[1] *T[2] *T[3] *T[4] *T[5] 
+        #q = self.jacotoDH(q)
+        q = self.DHtoJaco(q)
+        T_final = 1
+        #T_final = T_final * self.TAPI()
+        #T_final = T_final * self.TB() 
+        T_final = T_final * self.TAPI()
+        T_final = T_final * self.TJ1(q[0,0]) 
+        T_final = T_final * self.TJ2(q[0,1]) * self.TJ3(q[0,2])
+        T_final = T_final * self.TJ3Offset() * self.TJ4(q[0,3])
+        T_final = T_final * self.TJ5(q[0,4]) *self.TJ6(q[0,5]) * self.Tend()
         print T_final
         return T_final
         
     def jacotoDH(self, q):
-        q[0,0] = -q.item(0)
-        q[0,1] = q.item(1) + radians(90)
-        q[0,2] = q.item(2) - radians(90)
-        q[0,3] = q.item(3)
-        q[0,4] = q.item(4) + radians(180)
-        q[0,5] = q.item(5) - radians(180 - 80)
+        q[0,0] = -(q.item(0) - radians(180))
+        q[0,1] =  (q.item(1) - radians(270))
+        q[0,2] = -(q.item(2) - radians(90))
+        q[0,3] = -(q.item(3) - radians(180))
+        q[0,4] = -(q.item(4) - radians(180))
+        q[0,5] = -(q.item(5) - radians(180 + 80))
+        return q
+    def DHtoJaco(self, q):
+        q[0,0] = -q.item(0) + radians(180)
+        q[0,1] =  q.item(1) + radians(270)
+        q[0,2] = -q.item(2) + radians(90)
+        q[0,3] = -q.item(3) + radians(180)
+        q[0,4] = -q.item(4) + radians(180)
+        q[0,5] = -q.item(5) + radians(180 + 80)
         return q
         
         
@@ -95,8 +80,6 @@ class jacofk:
         # Initialize Node
         rospy.init_node('rbansal_srao_jacofk')
         
-        self.TBtoJB = self.transform(pi/2, 0, 0, 0) 
-        self.TJBtoJ1= self.transform(0, 0, 0, 0.028)
         # Setup publisher and Subscriber
         #self.endeffectorpose = rospy.Publisher('/map_OE', OccupancyGrid, latch=True)
         self.jointconfig = rospy.Subscriber('/jaco/joint_state', JointState , self.calcFK, queue_size=1)
