a386fba802e720613c0e037983df109d666eceac
Rohit Bansal <rohit@bansalrohit.com>
2014-04-29 12:35:43 -0400

    Follow trajectroy should work. Untested.

--- src/follow_traj.py
+++ src/follow_traj.py
@@ -8,47 +8,57 @@ import sys
 
 class follow_traj():
         
-    def pose_client(self):
-        client = actionlib.SimpleActionClient('/jaco/arm_pose', jaco_msgs.msg.ArmPoseAction)
-    
-        goal = jaco_msgs.msg.ArmPoseGoal()
-    
+    def traj_callback(self, msg):
+        px_traj = msg.posx_traj 
+        py_traj = msg.posy_traj 
+        pz_traj = msg.posz_traj 
+        
+        if(len(px_traj) != len(py_traj) or len(px_traj) != len(zx_traj)):
+            rospy.logwarn("The lengths of the trajectories don't match. Aborting")
+            return
+        else:
+            for i in xrange(0, len(px_traj)):
+                if(rospy.is_shutdown())
+                    rospy.logwarn("Shutdown request received")
+                    return 
+                self.pose_client(px_traj[i], py_traj[i], pz_traj[i])
+                
+        
+    def pose_client(self, x, y, z):
+        
         goal.pose.header.frame_id = "/jaco_api_origin"
-        pose = goal.pose.pose
-    
-        if len(sys.argv) < 8: # default pose
-            pose.position.x = -0.314269423485
-            pose.position.y = -0.339179039001
-            pose.position.z = 0.600132465363
-    
-            pose.orientation.x = -0.590686044496
-            pose.orientation.y = -0.519369415388
-            pose.orientation.z = 0.324703360925
-            pose.orientation.w = 0.525274342226
-    
-            rospy.logwarn("Using test goal: \n%s", goal)
-        else: # use pose from command line
-            pose.position.x = float(sys.argv[1])
-            pose.position.y = float(sys.argv[2])
-            pose.position.z = float(sys.argv[3])
-    
-            pose.orientation.x = float(sys.argv[4])
-            pose.orientation.y = float(sys.argv[5])
-            pose.orientation.z = float(sys.argv[6])
-            pose.orientation.w = float(sys.argv[7])
-    
+        
+        goal.pose.pose.orientation.x = -0.590686044496
+        goal.pose.pose.orientation.y = -0.519369415388
+        goal.pose.pose.orientation.z = 0.324703360925
+        goal.pose.pose.orientation.w = 0.525274342226
+        
+        
+        goal.pose.pose.position.x = x
+        goal.pose.pose.position.y = y
+        goal.pose.pose.position.z = z
+
         client.wait_for_server()
         rospy.loginfo("Connected to Pose server")
     
         client.send_goal(goal)
     
-        try:
-            client.wait_for_result()
-        except KeyboardInterrupt:
-            rospy.loginfo("Program interrupted, pre-empting goal")
-            client.cancel_all_goals()
+        return
     
-        return client.get_result()
+    def __init__(self):
+
+        # Initialize Node
+        rospy.init_node('rbansal_srao_follow_traj')
+        
+        self.px_traj = []
+        self.py_traj = []
+        self.pz_traj = []
+        
+        self.client = actionlib.SimpleActionClient('/jaco/arm_pose', jaco_msgs.msg.ArmPoseAction)
+        self.goal = jaco_msgs.msg.ArmPoseGoal()
+        
+        sub = rospy.Subscriber('/rbe_jacoapi/trajectories', trajectorymsg, self.traj_callback ,queue_size=1)
+        
 
 if __name__ == '__main__':
     try:
